183e43feec8c44802ae3c4a57a9e7115862e5958
sirvx <Artem.Kravchuk@infiz.khpi.edu.ua>
2024-10-11 14:00:54 +0300

    Реалізовано перешкоди та рахунок

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pygame import *
+import random
 
 init()
 
@@ -14,14 +15,13 @@ class GameSprite:
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+    def draw_rect(self):
+        draw.rect(window, (255, 0, 0), self.rect)
+
 
 class AnimatedSprite(GameSprite):
     def __init__(self, images, x, y, width, height):
         super().__init__(images[0], x, y, width, height)
-        # Використовуємо list comprehension для створення списку анімованих зображень
-        # Синтаксис [expression for item in iterable] дозволяє створити новий список на основі існуючого ітерабельного об'єкта
-        # У цьому випадку expression - це `transform.scale(image.load(img), (width, height))`, який масштабовує кожне зображення з `images`
-        # `for img in images` - це ітерація по всіх елементах у списку `images`, і кожне зображення обробляється та додається до нового списку
         self.images = [transform.scale(image.load(img), (width, height)) for img in images]
         self.current_image = 0
         self.animation_speed = 0.1
@@ -43,8 +43,18 @@ for i in range(-300, 601, 300):
     floor = GameSprite('img/floor.png', i, 400, 800, 100)
     floors.append(floor)
 
+cacti = [
+    GameSprite('img/cactus.png', random.randint(1100, 1500), 350, 40, 80),
+    GameSprite('img/cactus_big.png', random.randint(1300, 1700), 340, 60, 90)
+]
+
+restart_img = image.load('img/reset.png')
+
+font1 = font.Font('NFPixels-Regular.otf', 25)
+
 player_images = ['img/dino_run_1.png', 'img/dino_run_2.png']
 player = AnimatedSprite(player_images, 50, 340, 80, 100)
+player.rect.width -= 40
 
 jump = False
 jump_speed = 20
@@ -52,6 +62,10 @@ gravity = 1
 y_velocity = 0
 
 game = True
+game_speed = 8
+finish = False
+score = 0
+high_score = 0
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -60,24 +74,55 @@ while game:
             if e.key == K_SPACE and not jump:
                 jump = True
                 y_velocity = -jump_speed
+        if e.type == MOUSEBUTTONDOWN and finish:
+            finish = False
+            game_speed = 8
+            cacti = [
+                GameSprite('img/cactus.png', random.randint(1100, 1500), 350, 40, 80),
+                GameSprite('img/cactus_big.png', random.randint(1300, 1700), 340, 60, 90)
+            ]
+            player_images = ['img/dino_run_1.png', 'img/dino_run_2.png']
+            player = AnimatedSprite(player_images, 50, 340, 80, 100)
+            player.rect.width -= 40
+            score = 0
 
-    if jump:
-        player.rect.y += y_velocity
-        y_velocity += gravity
-        if player.rect.y >= 340:
-            player.rect.y = 340
-            jump = False
+    if not finish:
+        if jump:
+            player.rect.y += y_velocity
+            y_velocity += gravity
+            if player.rect.y >= 340:
+                player.rect.y = 340
+                jump = False
 
-    window.fill((255, 255, 255))
-    for floor in floors:
-        floor.rect.x -= 8
-        floor.reset()
-        if floor.rect.x < -550:
-            floor.rect.x = 800
+        window.fill((255, 255, 255))
+        for floor in floors:
+            floor.rect.x -= game_speed
+            floor.reset()
+            if floor.rect.x < -550:
+                floor.rect.x = 800
 
-    if not jump:
-        player.do_animate()
-    player.reset()
+        for cactus in cacti:
+            cactus.rect.x -= game_speed
+            cactus.reset()
+            if cactus.rect.x < -50:
+                cactus.rect.x = random.randint(1100, 1700)
+            if cactus.rect.colliderect(player.rect):
+                player_images = ['img/dino_game_over.png']
+                player = AnimatedSprite(player_images, player.rect.x, player.rect.y, 80, 100)
+                window.blit(restart_img, (330, 200))
+                finish = True
+
+        if not jump:
+            player.do_animate()
+
+        player.reset()
+        score += game_speed * 0.1
+        if score > high_score:
+            high_score = score
+        score_text = font1.render(f'{int(score)}/{int(high_score)}', True, (0, 0, 0))
+        window.blit(score_text, (680, 20))
+
+        game_speed += 0.001
 
     display.update()
     clock.tick(60)
